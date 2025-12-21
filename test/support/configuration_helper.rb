91d05826c0ec9951f6c139b75d58d0f8a0ddfeaa
eelco <git@eelcoj.com>
2025-12-21 11:47:51 +0700

    Remove `site_email` configuration

--- test/support/configuration_helper.rb
+++ test/support/configuration_helper.rb
@@ -5,7 +5,6 @@ module ConfigurationHelper
     setup do
       @default_attributes = {
         site_name: Perron.configuration.site_name,
-        site_email: Perron.configuration.site_email,
         default_url_options: Perron.configuration.default_url_options.dup,
         metadata: Perron.configuration.metadata.dup
       }
@@ -14,7 +13,6 @@ module ConfigurationHelper
     teardown do
       Perron.configure do |config|
         config.site_name = @default_attributes[:site_name]
-        config.site_email = @default_attributes[:site_email]
         config.default_url_options = @default_attributes[:default_url_options]
         config.metadata = @default_attributes[:metadata]
       end
