132582686ea3b1b1d398cbc485b7f5ca921d6b29
e <988051+eelcoj@users.noreply.github.com>
2026-01-05 14:14:46 +0000

    Add OutputServer middleware to serve /output/ folder (#88)
    Adds middleware that serves static files from the output directory before
    passing requests to Rails routing. Automatically enabled when output directory exists.
    
    Also adds `perron:clobber` task to remove generated output.

--- lib/perron/engine.rb
+++ lib/perron/engine.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
+require "perron/output_server"
+
 module Perron
   class Engine < Rails::Engine
     initializer "perron.default_url_options" do |app|
       app.config.action_controller.default_url_options = Perron.configuration.default_url_options
     end
 
+    initializer "perron.output_server" do |app|
+      app.middleware.use Perron::OutputServer
+    end
+
     rake_tasks do
       load File.expand_path("../tasks/build.rake", __FILE__)
-      load File.expand_path("../tasks/validate.rake", __FILE__)
+      load File.expand_path("../tasks/clobber.rake", __FILE__)
       load File.expand_path("../tasks/sync_sources.rake", __FILE__)
+      load File.expand_path("../tasks/validate.rake", __FILE__)
     end
   end
 end
