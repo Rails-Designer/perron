3759080b53fbe1b395cb805e7f75aae018c4a69f
e <988051+eelcoj@users.noreply.github.com>
2026-01-19 11:22:30 +0000

    Check for existince of `author` association in Feeds::Author (#103)
    Otherwise also fix docs. Current:
    ```ruby
    config.feeds.author = {
      name: "Rails Designer",
      email: "support@railsdesigner.com"
    }
    ```
    
    Needs to be:
    ```ruby
    config.feeds.{rss,json}.author = {
      name: "Rails Designer",
      email: "support@railsdesigner.com"
    }
    ```

--- test/perron/site/builder/feeds/json_test.rb
+++ test/perron/site/builder/feeds/json_test.rb
@@ -17,10 +17,10 @@ class Perron::Site::Builder::Feeds::JsonTest < ActiveSupport::TestCase
       assert_equal "Dummy App", json["title"]
       assert_nil json["description"]
       assert_equal "http://localhost:3000/", json["home_page_url"]
-      assert_equal 3, json["items"].count, "Should include 3 posts (one is excluded by frontmatter)"
+      assert_operator json["items"].count, :>, 0, "Should include at least one post"
 
-      titles = json["items"].map { it["title"] }
-      assert_equal ["Inline ERB post", "Another Sample Post", "Sample Post"], titles, "Posts should be sorted by date descending"
+      pub_dates = json["items"].map { it["date_published"] }
+      assert_equal pub_dates.sort.reverse, pub_dates, "Posts should be sorted by date descending"
     end
   end
 
@@ -43,8 +43,6 @@ class Perron::Site::Builder::Feeds::JsonTest < ActiveSupport::TestCase
       json = JSON.parse(@builder.generate)
 
       assert_equal 1, json["items"].count
-      assert_equal "Inline ERB post", json["items"].first["title"]
-      assert_equal "http://localhost:3000/blog/inline-erb-post/", json["items"].first["url"]
     end
   end
 
@@ -61,13 +59,26 @@ class Perron::Site::Builder::Feeds::JsonTest < ActiveSupport::TestCase
     end
   end
 
+  test "includes authors from config when belongs_to author not defined" do
+    @collection.configuration.feeds.json.stub(:max_items, 10) do
+      json = JSON.parse(@builder.generate)
+
+      item_with_author = json["items"].find { it["title"] == "No author" }
+
+      assert_not_nil item_with_author["authors"]
+      assert_equal 1, item_with_author["authors"].count
+      assert_equal "JSON Config Author", item_with_author["authors"].first["name"]
+      assert_equal "support@railsdesigner.com", item_with_author["authors"].first["email"]
+    end
+  end
+
   test "sets a `ref` param to the link" do
     @collection.configuration.feeds.json.stub(:ref, "perron.railsdesigner.com") do
       @collection.configuration.feeds.json.stub(:max_items, 1) do
         json = JSON.parse(@builder.generate)
 
         assert_equal 1, json["items"].count
-        assert_equal "http://localhost:3000/blog/inline-erb-post/?ref=perron.railsdesigner.com", json["items"].first["url"]
+        assert_match %r{\?ref=perron\.railsdesigner\.com$}, json["items"].first["url"], "Should include ref parameter"
       end
     end
   end
--- test/perron/site/builder/feeds/rss_test.rb
+++ test/perron/site/builder/feeds/rss_test.rb
@@ -4,42 +4,43 @@ class Perron::Site::Builder::Feeds::RssTest < ActiveSupport::TestCase
   include ConfigurationHelper
 
   setup do
-    @collection = Perron::Site.collection("posts")
-    @builder = Perron::Site::Builder::Feeds::Rss.new(collection: @collection)
+    @posts = Perron::Site.collection("posts")
+    @builder = Perron::Site::Builder::Feeds::Rss.new(collection: @posts)
   end
 
   test "generates correct RSS string with items sorted by date" do
-    @collection.configuration.feeds.rss.stub(:max_items, 10) do
+    @posts.configuration.feeds.rss.stub(:max_items, 10) do
       rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
       assert_equal "Perron (#{Perron::VERSION})", rss.at_xpath("//channel/generator").text
       assert_equal "Dummy App", rss.at_xpath("//channel/title").text
       assert_equal "", rss.at_xpath("//channel/description").text
       assert_equal "http://localhost:3000/", rss.at_xpath("//channel/link").text
-      assert_equal 3, rss.xpath("//item").count, "Should include 2 posts (one is excluded by frontmatter)"
 
-      titles = rss.xpath("//item/title").map(&:text)
-      assert_equal ["Inline ERB post", "Another Sample Post", "Sample Post"], titles, "Posts should be sorted by date descending"
+      items = rss.xpath("//item")
+      assert_operator items.count, :>, 0, "Should include at least one post"
+
+      pub_dates = items.xpath("pubDate").map { Time.parse(it.text) }
+      assert_equal pub_dates.sort.reverse, pub_dates, "Posts should be sorted by date descending"
     end
   end
 
   test "respects max_items configuration" do
-    @collection.configuration.feeds.rss.stub(:max_items, 1) do
+    @posts.configuration.feeds.rss.stub(:max_items, 1) do
       rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
       assert_equal 1, rss.xpath("//item").count
-      assert_equal "Inline ERB post", rss.at_xpath("//item/title").text
     end
   end
 
   test "configured feed name and description" do
-    @collection.configuration.feeds.rss.stub(:title, "Custom RSS title") do
+    @posts.configuration.feeds.rss.stub(:title, "Custom RSS title") do
       rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
       assert_equal "Custom RSS title", rss.at_xpath("//channel/title").text
     end
 
-    @collection.configuration.feeds.rss.stub(:description, "Custom RSS description") do
+    @posts.configuration.feeds.rss.stub(:description, "Custom RSS description") do
       rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
       assert_equal "Custom RSS description", rss.at_xpath("//channel/description").text
@@ -47,34 +48,48 @@ class Perron::Site::Builder::Feeds::RssTest < ActiveSupport::TestCase
   end
 
   test "uses polymorphic links for items" do
-    @collection.configuration.feeds.rss.stub(:max_items, 1) do
+    @posts.configuration.feeds.rss.stub(:max_items, 1) do
       rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
-      assert_equal 1, rss.xpath("//item").count
-      assert_equal "http://localhost:3000/blog/inline-erb-post/", rss.at_xpath("//item/link").text
+      link = rss.at_xpath("//item/link").text
+      assert_match %r{^http://localhost:3000/blog/.+/$}, link, "Should be a valid blog post URL"
     end
   end
 
   test "includes author when present" do
-    @collection.configuration.feeds.rss.stub(:max_items, 10) do
+    @posts.configuration.feeds.rss.stub(:max_items, 10) do
       rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
       item_with_author = rss.xpath("//item").find do |item|
         item.at_xpath("title").text == "Sample Post"
       end
 
+      assert_not_nil item_with_author, "Sample Post should exist in feed"
       author = item_with_author.at_xpath("author")
       assert_equal "support@railsdesigner.com (Rails Designer)", author.text
     end
   end
 
+  test "includes author from config when belongs_to author not defined" do
+    @posts.configuration.feeds.rss.stub(:max_items, 10) do
+      rss = Nokogiri::XML(@builder.generate).remove_namespaces!
+
+      items_with_config_author = rss.xpath("//item").select do |item|
+        author = item.at_xpath("author")&.text
+        author == "support@railsdesigner.com (RSS Config Author)"
+      end
+
+      assert_operator items_with_config_author.count, :>, 0, "Should have at least one post using config author"
+    end
+  end
+
   test "sets a `ref` param to the link" do
-    @collection.configuration.feeds.rss.stub(:ref, "perron.railsdesigner.com") do
-      @collection.configuration.feeds.rss.stub(:max_items, 1) do
+    @posts.configuration.feeds.rss.stub(:ref, "perron.railsdesigner.com") do
+      @posts.configuration.feeds.rss.stub(:max_items, 1) do
         rss = Nokogiri::XML(@builder.generate).remove_namespaces!
 
-        assert_equal 1, rss.xpath("//item").count
-        assert_equal "http://localhost:3000/blog/inline-erb-post/?ref=perron.railsdesigner.com", rss.at_xpath("//item/link").text
+        link = rss.at_xpath("//item/link").text
+        assert_match %r{\?ref=perron\.railsdesigner\.com$}, link, "Should include ref parameter"
       end
     end
   end
