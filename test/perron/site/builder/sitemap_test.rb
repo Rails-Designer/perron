ec8d3e849faf81e4ffde2a8ba9bb502b9541d8f3
Mathieu LAGÜE <mathieu@lague.me>
2026-02-25 11:15:00 +0100

    Fix sitemap <lastmod> missing when metadata.updated_at is absent (#145)
    * Fix sitemap <lastmod> missing when metadata.updated_at is absent
    
    * Skip `lastmod` tag when `metadata.updated_at` is not present
    
    ---------
    
    Co-authored-by: eelco <gitt@eelcoj.com>

--- test/perron/site/builder/sitemap_test.rb
+++ test/perron/site/builder/sitemap_test.rb
@@ -36,4 +36,24 @@ class Perron::Site::Builder::SitemapTest < ActiveSupport::TestCase
     assert_includes urls, "http://#{host}/about/"
     refute_includes urls, "http://#{host}/blog/"
   end
+
+  test "sitemap uses resource updated_at as lastmod when present" do
+    Perron.configuration.sitemap.enabled = true
+    Perron::Site::Builder::Sitemap.new(Rails.root.join("output")).generate
+
+    sitemap = Nokogiri::XML(File.read("test/dummy/output/sitemap.xml")).tap(&:remove_namespaces!)
+
+    sample_post_lastmod = sitemap.xpath("//url[loc[contains(text(),'sample-post')]]/lastmod").text
+    assert_equal "2023-05-15", sample_post_lastmod
+  end
+
+  test "sitemap omits lastmod when updated_at is missing" do
+    Perron.configuration.sitemap.enabled = true
+    Perron::Site::Builder::Sitemap.new(Rails.root.join("output")).generate
+
+    sitemap = Nokogiri::XML(File.read("test/dummy/output/sitemap.xml")).tap(&:remove_namespaces!)
+
+    another_post_lastmod = sitemap.xpath("//url[loc[contains(text(),'another-post')]]/lastmod")
+    assert_empty another_post_lastmod, "lastmod should not be present when updated_at is missing"
+  end
 end
