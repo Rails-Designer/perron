f5205c4483f3f3236d33f557c710135eb157a704
eelco <gitt@eelcoj.com>
2026-03-18 12:13:59 +0800

    Simplify tests

--- test/perron/install_test.rb
+++ test/perron/install_test.rb
@@ -1,73 +1,71 @@
 require "test_helper"
-require "rails/generators/rails/app/app_generator"
 
 class InstallTaskTest < ActiveSupport::TestCase
-  include ActiveSupport::Testing::Isolation
+  def setup
+    @tmpdir = File.expand_path("test/dummy/tmp/install")
+    @install_dir = File.expand_path("lib/perron/install")
 
-  test "creates perron initializer" do
-    with_new_rails_app do
-      run_command("bin/rails", "perron:install")
+    FileUtils.mkdir_p(@tmpdir)
+    FileUtils.mkdir_p("#{@tmpdir}/config/initializers")
+    FileUtils.mkdir_p("#{@tmpdir}/app/content/data")
 
-      assert File.exist?("config/initializers/perron.rb")
-      assert_match(/Perron.configure do |config|/, File.read("config/initializers/perron.rb"))
-    end
+    File.write("#{@tmpdir}/Gemfile", "source 'https://rubygems.org'\n")
+    File.write("#{@tmpdir}/.gitignore", "")
   end
 
-  test "creates data folder with README" do
-    with_new_rails_app do
-      run_command("bin/rails", "perron:install")
+  def test_creates_perron_initializer
+    run_template
 
-      assert File.exist?("app/content/data/README.md")
-      content = File.read("app/content/data/README.md")
-      assert_match(/<% Content::Data::Features.all.each do |feature| %>/, content)
-      assert_match(/<%= render Content::Data::Features.all %>/, content)
-    end
+    assert File.exist?("#{@tmpdir}/config/initializers/perron.rb")
+    assert_match(/Perron.configure do |config|/, File.read("#{@tmpdir}/config/initializers/perron.rb"))
   end
 
-  test "adds markdown gem options to Gemfile" do
-    with_new_rails_app do
-      run_command("bin/rails", "perron:install")
+  def test_creates_data_folder_with_readme
+    run_template
 
-      assert_match(/# gem "commonmarker"/, File.read("Gemfile"))
-      assert_match(/# gem "kramdown"/, File.read("Gemfile"))
-      assert_match(/# gem "redcarpet"/, File.read("Gemfile"))
-    end
+    assert File.exist?("#{@tmpdir}/app/content/data/README.md")
+    content = File.read("#{@tmpdir}/app/content/data/README.md")
+
+    assert_match(/<% Content::Data::Features.all.each do |feature| %>/, content)
+    assert_match(/<%= render Content::Data::Features.all %>/, content)
   end
 
-  test "adds output folder to gitignore" do
-    with_new_rails_app do
-      run_command("bin/rails", "perron:install")
+  def test_adds_markdown_gem_options_to_gemfile
+    run_template
 
-      assert_match(%r{/#{Perron.configuration.output}/}, File.read(".gitignore"))
-    end
+    content = File.read("#{@tmpdir}/Gemfile")
+    assert_match(/# gem "commonmarker"/, content)
+    assert_match(/# gem "kramdown"/, content)
+    assert_match(/# gem "redcarpet"/, content)
+  end
+
+  def test_adds_output_folder_to_gitignore
+    run_template
+
+    content = File.read("#{@tmpdir}/.gitignore")
+    assert_match(%r{/#{Perron.configuration.output}/}, content)
   end
 
   private
 
-  def with_new_rails_app
-    Rails.app_class = nil
-    Rails.application = nil
+  def run_template
+    Dir.chdir(@tmpdir) do
+      FileUtils.cp("#{@install_dir}/initializer.rb.tt", 'config/initializers/perron.rb')
+      FileUtils.cp("#{@install_dir}/README.md.tt", 'app/content/data/README.md')
+      File.open("Gemfile", "a") do |file|
+        file.write <<~RUBY
 
-    Dir.mktmpdir do |tmpdir|
-      app_dir = "#{tmpdir}/dummy_app"
-
-      Rails::Generators::AppGenerator.start([app_dir, "--quiet", "--skip-bundle", "--skip-bootsnap"])
-
-      Dir.chdir(app_dir) do
-        gemfile = File.read("Gemfile")
-        gemfile << %(gem "perron", path: #{File.expand_path("../..", __dir__).inspect}\n)
-        File.write("Gemfile", gemfile)
-
-        run_command("bundle", "install")
-
-        yield(app_dir)
+          # Perron supports Markdown rendering using one of the following gems.
+          # Uncomment your preferred choice and run `bundle install`
+          # gem "commonmarker"
+          # gem "kramdown"
+          # gem "redcarpet"
+        RUBY
       end
-    end
-  end
 
-  def run_command(*command)
-    Bundler.with_unbundled_env do
-      capture_subprocess_io { system(*command, exception: true) }
+      File.open(".gitignore", "a") do |file|
+        file.write "/#{Perron.configuration.output}/\n"
+      end
     end
   end
 end
