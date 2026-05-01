7ff609a9b8d1bcf3586c51cb1bc813c8b09842c9
eelco <gitt@eelcoj.com>
2026-05-01 15:14:18 +0530

    Limit command to run by concierge

--- app/controllers/perron/concierge_controller.rb
+++ app/controllers/perron/concierge_controller.rb
@@ -5,8 +5,11 @@ module Perron
     end
 
     def run_command
-      system(params[:command])
+      command = params[:command]
 
+      return redirect_back fallback_location: root_path unless command.start_with?("bin/rails generate content")
+
+      system(command)
       redirect_back fallback_location: root_path
     end
   end
